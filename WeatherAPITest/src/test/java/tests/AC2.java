a974b65238ea08d6f80122e881a9d9672e47b2c9
RVMac <rovenmacapagal@gmail.com>
2022-01-16 23:41:05 +0800

    added AC2

--- WeatherAPITest/src/test/java/tests/AC2.java
+++ WeatherAPITest/src/test/java/tests/AC2.java
@@ -1,5 +1,25 @@
 package tests;
 
-public class AC2 {
+import org.testng.annotations.Test;
 
+import utilities.commonFunctions;
+
+public class AC2 extends BaseClass{
+	String cityID = "4487042";
+	String countryCode = "US";
+	String stateCode = "NC";
+	String cityName = "Raleigh";
+	
+	String incorrectCityID = "9999999";
+	@Test
+	void test_01_correctCity() {
+		int statusCode = 200;
+		commFunc.getCurrentWeatherCity(statusCode, cityID, countryCode, stateCode, cityName);
+	}
+	
+	@Test
+	void test_02_incorrectCity() {
+		int statusCode = 204;
+		commFunc.getCurrentWeatherStatusCode(incorrectCityID, statusCode);
+	}
 }
